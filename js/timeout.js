bbc2586002da686ac19c611975a8fb39b2aa7281
IAguadoHdez <iaguado20938@iesjoanramis.org>
2025-06-06 19:37:35 +0200

    He quitado el login.js de mis html pq me saltaba un error y no afecta a nada si lo quito  y he creado el timeout de 2min

--- js/timeout.js
+++ js/timeout.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
     const BASE_URL = window.location.hostname === "127.0.0.1" ? "" : "/IkerAguado_ProyectoLGMSI";
-  const Tiempo_max_inactivo = 2 * 6 * 1000;
+  const Tiempo_max_inactivo = 2 * 60 * 1000;
   let temporizador;
 
 
   function cerrarSesion(){
+    alert("Has sido desconectado por inactividad");
     localStorage.removeItem("usuarioActual");
     location.href = `${BASE_URL}/paginas/login.html`;
   }
@@ -14,6 +15,10 @@ document.addEventListener("DOMContentLoaded", function(){
     temporizador = setTimeout(cerrarSesion,Tiempo_max_inactivo);
   }
 
+  document.addEventListener("mousemove",reiniciarTemporizador);
+  document.addEventListener("keydown",reiniciarTemporizador);
+  document.addEventListener("click",reiniciarTemporizador);
+  document.addEventListener("scroll",reiniciarTemporizador);
   
 
   reiniciarTemporizador();
